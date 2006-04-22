4affbdb1a93df50c2d2974bf5b67777784150ad0
zerohero <zerohero@b88df514-890a-458d-8813-3e1211de039a>
2006-04-22 14:42:59 +0000

    remove

--- dopus4-old/dopusrt-os4/DOpusRT_rev.h
+++
@@ -1,6 +0,0 @@
-#define VERSION		4
-#define REVISION	8
-#define DATE		__DATE__
-#define VERS		"DOpusRT 4.8"
-#define VSTRING		"DOpusRT 4.8 (" __DATE__ ")\r\n"
-#define VERSTAG		"\0$VER: DOpusRT 4.8 (" __DATE__ ")"
