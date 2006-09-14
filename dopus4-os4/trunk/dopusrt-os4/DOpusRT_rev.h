f9b4ce49faa46a905d5002c26be678e314502678
gilfanon <gilfanon@b88df514-890a-458d-8813-3e1211de039a>
2006-09-14 22:51:38 +0000

    'revbump <file> sub' used as default with 'make revision' now.

--- dopus4-os4/trunk/dopusrt-os4/DOpusRT_rev.h
+++ dopus4-os4/trunk/dopusrt-os4/DOpusRT_rev.h
@@ -1,16 +1,16 @@
 #define VERSION			4
 #define REVISION		9
-#define SUBREVISION		3
+#define SUBREVISION		4
 
-#define DATE			"12.09.2006"
+#define DATE			"15.09.2006"
 #define VERS			"DOpusRT 4.9"
-#define VSTRING			"DOpusRT 4.9 (12.09.2006)\r\n"
-#define VERSTAG			"\0$VER: DOpusRT 4.9 (12.09.2006)"
+#define VSTRING			"DOpusRT 4.9 (15.09.2006)\r\n"
+#define VERSTAG			"\0$VER: DOpusRT 4.9 (15.09.2006)"
 
 #define VERSION_STR		"4"
 #define REVISION_STR		"9"
-#define SUBREVISION_STR		"3"
+#define SUBREVISION_STR		"4"
 
-#define NEW_VERS		"DOpusRT 4.9.3"
-#define NEW_VSTRING		"DOpusRT 4.9.3 (12.09.2006)\r\n"
-#define NEW_VERSTAG		"\0$VER: DOpusRT 4.9.3 (12.09.2006)"
+#define NEW_VERS		"DOpusRT 4.9.4"
+#define NEW_VSTRING		"DOpusRT 4.9.4 (15.09.2006)\r\n"
+#define NEW_VERSTAG		"\0$VER: DOpusRT 4.9.4 (15.09.2006)"
