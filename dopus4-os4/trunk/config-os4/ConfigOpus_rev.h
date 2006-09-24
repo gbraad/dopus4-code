107009fb24f20a355254ca919837a87436b47851
gilfanon <gilfanon@b88df514-890a-458d-8813-3e1211de039a>
2006-09-24 14:49:15 +0000

    Bump.

--- dopus4-os4/trunk/config-os4/ConfigOpus_rev.h
+++ dopus4-os4/trunk/config-os4/ConfigOpus_rev.h
@@ -1,16 +1,16 @@
 #define VERSION			4
 #define REVISION		17
-#define SUBREVISION		8
+#define SUBREVISION		9
 
-#define DATE			"19.09.2006"
+#define DATE			"24.09.2006"
 #define VERS			"ConfigOpus 4.17"
-#define VSTRING			"ConfigOpus 4.17 (19.09.2006)\r\n"
-#define VERSTAG			"\0$VER: ConfigOpus 4.17 (19.09.2006)"
+#define VSTRING			"ConfigOpus 4.17 (24.09.2006)\r\n"
+#define VERSTAG			"\0$VER: ConfigOpus 4.17 (24.09.2006)"
 
 #define VERSION_STR		"4"
 #define REVISION_STR		"17"
-#define SUBREVISION_STR		"8"
+#define SUBREVISION_STR		"9"
 
-#define NEW_VERS		"ConfigOpus 4.17.8"
-#define NEW_VSTRING		"ConfigOpus 4.17.8 (19.09.2006)\r\n"
-#define NEW_VERSTAG		"\0$VER: ConfigOpus 4.17.8 (19.09.2006)"
+#define NEW_VERS		"ConfigOpus 4.17.9"
+#define NEW_VSTRING		"ConfigOpus 4.17.9 (24.09.2006)\r\n"
+#define NEW_VERSTAG		"\0$VER: ConfigOpus 4.17.9 (24.09.2006)"
