8a81f589523ef18d634bd224aba2a46d674607c2
gilfanon <gilfanon@b88df514-890a-458d-8813-3e1211de039a>
2006-09-28 19:49:30 +0000

    Complete removal of IDOpus->Assign();

--- dopus4-os4/trunk/library-os4/dopus.library_rev.h
+++ dopus4-os4/trunk/library-os4/dopus.library_rev.h
@@ -1,16 +1,16 @@
 #define VERSION			22
-#define REVISION		32
+#define REVISION		33
 #define SUBREVISION		0
 
 #define DATE			"28.09.2006"
-#define VERS			"dopus.library 22.32"
-#define VSTRING			"dopus.library 22.32 (28.09.2006)\r\n"
-#define VERSTAG			"\0$VER: dopus.library 22.32 (28.09.2006)"
+#define VERS			"dopus.library 22.33"
+#define VSTRING			"dopus.library 22.33 (28.09.2006)\r\n"
+#define VERSTAG			"\0$VER: dopus.library 22.33 (28.09.2006)"
 
 #define VERSION_STR		"22"
-#define REVISION_STR		"32"
+#define REVISION_STR		"33"
 #define SUBREVISION_STR		"0"
 
-#define NEW_VERS		"dopus.library 22.32"
-#define NEW_VSTRING		"dopus.library 22.32 (28.09.2006)\r\n"
-#define NEW_VERSTAG		"\0$VER: dopus.library 22.32 (28.09.2006)"
+#define NEW_VERS		"dopus.library 22.33"
+#define NEW_VSTRING		"dopus.library 22.33 (28.09.2006)\r\n"
+#define NEW_VERSTAG		"\0$VER: dopus.library 22.33 (28.09.2006)"
