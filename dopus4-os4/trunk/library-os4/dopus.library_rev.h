0a3e52dbb2deb77ece23d71b582245b5f2626517
gilfanon <gilfanon@b88df514-890a-458d-8813-3e1211de039a>
2006-09-28 19:17:01 +0000

    Removal of IDOpus->Atoh(); and IDOpus->PathName(); completely.

--- dopus4-os4/trunk/library-os4/dopus.library_rev.h
+++ dopus4-os4/trunk/library-os4/dopus.library_rev.h
@@ -1,16 +1,16 @@
 #define VERSION			22
-#define REVISION		31
+#define REVISION		32
 #define SUBREVISION		0
 
-#define DATE			"27.09.2006"
-#define VERS			"dopus.library 22.31"
-#define VSTRING			"dopus.library 22.31 (27.09.2006)\r\n"
-#define VERSTAG			"\0$VER: dopus.library 22.31 (27.09.2006)"
+#define DATE			"28.09.2006"
+#define VERS			"dopus.library 22.32"
+#define VSTRING			"dopus.library 22.32 (28.09.2006)\r\n"
+#define VERSTAG			"\0$VER: dopus.library 22.32 (28.09.2006)"
 
 #define VERSION_STR		"22"
-#define REVISION_STR		"31"
+#define REVISION_STR		"32"
 #define SUBREVISION_STR		"0"
 
-#define NEW_VERS		"dopus.library 22.31"
-#define NEW_VSTRING		"dopus.library 22.31 (27.09.2006)\r\n"
-#define NEW_VERSTAG		"\0$VER: dopus.library 22.31 (27.09.2006)"
+#define NEW_VERS		"dopus.library 22.32"
+#define NEW_VSTRING		"dopus.library 22.32 (28.09.2006)\r\n"
+#define NEW_VERSTAG		"\0$VER: dopus.library 22.32 (28.09.2006)"
--- dopus4-os4/trunk/library-os4/dopus_vectors.c
+++ dopus4-os4/trunk/library-os4/dopus_vectors.c
@@ -46,7 +46,6 @@ extern int                  VARARGS68K _DOpus_LStrnCmpI(struct DOpusIFace *, cha
 extern int                  VARARGS68K _DOpus_StrCombine(struct DOpusIFace *, char * s1, char * s2, char * s3, int len);
 extern int                  VARARGS68K _DOpus_StrConcat(struct DOpusIFace *, char * s1, char * s2, int len);
 extern int                  VARARGS68K _DOpus_Assign(struct DOpusIFace *, char * name, char * dir);
-extern int                  VARARGS68K _DOpus_PathName(struct DOpusIFace *, BPTR lock, char * buffer, int len);
 extern int                  VARARGS68K _DOpus_TackOn(struct DOpusIFace *, char * path, char * name, int len);
 extern void                 VARARGS68K _DOpus_StampToStr(struct DOpusIFace *, struct DateTime * datetime);
 extern int                  VARARGS68K _DOpus_StrToStamp(struct DOpusIFace *, struct DateTime * datetime);
@@ -133,7 +132,6 @@ STATIC CONST APTR main_vectors[] =
     _DOpus_StrCombine,
     _DOpus_StrConcat,
     _DOpus_Assign,
-    _DOpus_PathName,
     _DOpus_TackOn,
     _DOpus_StampToStr,
     _DOpus_StrToStamp,
