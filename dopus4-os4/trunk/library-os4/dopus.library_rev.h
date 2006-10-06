66eae455c2bfc33da8b2867e2d2d0ed76ca83316
gilfanon <gilfanon@b88df514-890a-458d-8813-3e1211de039a>
2006-10-06 22:47:40 +0000

    Revisionbump.

--- dopus4-os4/trunk/library-os4/dopus.library_rev.h
+++ dopus4-os4/trunk/library-os4/dopus.library_rev.h
@@ -1,16 +1,16 @@
 #define VERSION			22
-#define REVISION		36
+#define REVISION		37
 #define SUBREVISION		0
 
-#define DATE			"04.10.2006"
-#define VERS			"dopus.library 22.36"
-#define VSTRING			"dopus.library 22.36 (04.10.2006)\r\n"
-#define VERSTAG			"\0$VER: dopus.library 22.36 (04.10.2006)"
+#define DATE			"07.10.2006"
+#define VERS			"dopus.library 22.37"
+#define VSTRING			"dopus.library 22.37 (07.10.2006)\r\n"
+#define VERSTAG			"\0$VER: dopus.library 22.37 (07.10.2006)"
 
 #define VERSION_STR		"22"
-#define REVISION_STR		"36"
+#define REVISION_STR		"37"
 #define SUBREVISION_STR		"0"
 
-#define NEW_VERS		"dopus.library 22.36"
-#define NEW_VSTRING		"dopus.library 22.36 (04.10.2006)\r\n"
-#define NEW_VERSTAG		"\0$VER: dopus.library 22.36 (04.10.2006)"
+#define NEW_VERS		"dopus.library 22.37"
+#define NEW_VSTRING		"dopus.library 22.37 (07.10.2006)\r\n"
+#define NEW_VERSTAG		"\0$VER: dopus.library 22.37 (07.10.2006)"
