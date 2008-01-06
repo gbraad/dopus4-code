e27d35f23c3641339bd6d159dc9758873abebe2c
gilfanon <gilfanon@b88df514-890a-458d-8813-3e1211de039a>
2008-01-06 23:15:02 +0000

    Small changes.

--- dopus4-os4/trunk/library-os4/dopus.library_rev.h
+++ dopus4-os4/trunk/library-os4/dopus.library_rev.h
@@ -1,16 +1,16 @@
 #define VERSION			22
-#define REVISION		39
+#define REVISION		40
 #define SUBREVISION		0
 
-#define DATE			"27.06.2007"
-#define VERS			"dopus.library 22.39"
-#define VSTRING			"dopus.library 22.39 (27.06.2007)\r\n"
-#define VERSTAG			"\0$VER: dopus.library 22.39 (27.06.2007)"
+#define DATE			"07.01.2008"
+#define VERS			"dopus.library 22.40"
+#define VSTRING			"dopus.library 22.40 (07.01.2008)\r\n"
+#define VERSTAG			"\0$VER: dopus.library 22.40 (07.01.2008)"
 
 #define VERSION_STR		"22"
-#define REVISION_STR		"39"
+#define REVISION_STR		"40"
 #define SUBREVISION_STR		"0"
 
-#define NEW_VERS		"dopus.library 22.39"
-#define NEW_VSTRING		"dopus.library 22.39 (27.06.2007)\r\n"
-#define NEW_VERSTAG		"\0$VER: dopus.library 22.39 (27.06.2007)"
+#define NEW_VERS		"dopus.library 22.40"
+#define NEW_VSTRING		"dopus.library 22.40 (07.01.2008)\r\n"
+#define NEW_VERSTAG		"\0$VER: dopus.library 22.40 (07.01.2008)"
