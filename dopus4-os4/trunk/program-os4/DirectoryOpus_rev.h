da3536f17d03b2031c894c858319042fd4f05a7f
gilfanon <gilfanon@b88df514-890a-458d-8813-3e1211de039a>
2008-10-22 22:45:23 +0000

    Revision bump.

--- dopus4-os4/trunk/program-os4/DirectoryOpus_rev.h
+++ dopus4-os4/trunk/program-os4/DirectoryOpus_rev.h
@@ -1,16 +1,16 @@
 #define VERSION			4
 #define REVISION		18
-#define SUBREVISION		13
+#define SUBREVISION		14
 
-#define DATE			"21.10.2008"
+#define DATE			"23.10.2008"
 #define VERS			"DirectoryOpus 4.18"
-#define VSTRING			"DirectoryOpus 4.18 (21.10.2008)\r\n"
-#define VERSTAG			"\0$VER: DirectoryOpus 4.18 (21.10.2008)"
+#define VSTRING			"DirectoryOpus 4.18 (23.10.2008)\r\n"
+#define VERSTAG			"\0$VER: DirectoryOpus 4.18 (23.10.2008)"
 
 #define VERSION_STR		"4"
 #define REVISION_STR		"18"
-#define SUBREVISION_STR		"13"
+#define SUBREVISION_STR		"14"
 
-#define NEW_VERS		"DirectoryOpus 4.18.13"
-#define NEW_VSTRING		"DirectoryOpus 4.18.13 (21.10.2008)\r\n"
-#define NEW_VERSTAG		"\0$VER: DirectoryOpus 4.18.13 (21.10.2008)"
+#define NEW_VERS		"DirectoryOpus 4.18.14"
+#define NEW_VSTRING		"DirectoryOpus 4.18.14 (23.10.2008)\r\n"
+#define NEW_VERSTAG		"\0$VER: DirectoryOpus 4.18.14 (23.10.2008)"
