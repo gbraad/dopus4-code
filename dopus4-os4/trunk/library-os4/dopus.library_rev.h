5e44a6994d7a6967210d0028263dbf11fd9ca761
paraxenic <paraxenic@b88df514-890a-458d-8813-3e1211de039a>
2012-08-25 20:50:44 +0000

    Updated library revision since changes were made.

--- dopus4-os4/trunk/library-os4/dopus.library_rev.h
+++ dopus4-os4/trunk/library-os4/dopus.library_rev.h
@@ -1,16 +1,16 @@
 #define VERSION			22
-#define REVISION		40
+#define REVISION		41
 #define SUBREVISION		0
 
-#define DATE			"07.01.2008"
-#define VERS			"dopus.library 22.40"
-#define VSTRING			"dopus.library 22.40 (07.01.2008)\r\n"
-#define VERSTAG			"\0$VER: dopus.library 22.40 (07.01.2008)"
+#define DATE			"25.08.2012"
+#define VERS			"dopus.library 22.41"
+#define VSTRING			"dopus.library 22.41 (25.08.2012)\r\n"
+#define VERSTAG			"\0$VER: dopus.library 22.41 (25.08.2012)"
 
 #define VERSION_STR		"22"
-#define REVISION_STR		"40"
+#define REVISION_STR		"41"
 #define SUBREVISION_STR		"0"
 
-#define NEW_VERS		"dopus.library 22.40"
-#define NEW_VSTRING		"dopus.library 22.40 (07.01.2008)\r\n"
-#define NEW_VERSTAG		"\0$VER: dopus.library 22.40 (07.01.2008)"
+#define NEW_VERS		"dopus.library 22.41"
+#define NEW_VSTRING		"dopus.library 22.41 (25.08.2012)\r\n"
+#define NEW_VERSTAG		"\0$VER: dopus.library 22.41 (25.08.2012)"
