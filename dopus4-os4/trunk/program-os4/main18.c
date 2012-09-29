c8832ed8151397146cd0d0117decb5158304d6c2
paraxenic <paraxenic@b88df514-890a-458d-8813-3e1211de039a>
2012-09-29 03:47:37 +0000

    Added DevNameFromLock() function call to eliminate errors when copying relative directory soft links. Corrected initial fix.

--- dopus4-os4/trunk/program-os4/main18.c
+++ dopus4-os4/trunk/program-os4/main18.c
@@ -123,9 +123,16 @@ int recursedir(STRPTR fdir, STRPTR fdest, int dowhat, int fdata)
 	dest = dir + 2048;
 	dname = dest + 2048;
 	ddir = dname + 2048;
+
 	if(fdir) //temp fix for relative link copy failure
-		if (!(IDOS->DevNameFromLock(mylock, dir, 2048, DN_FULLPATH)))
+		if (mylock)
+		{
+			if (!(IDOS->DevNameFromLock(mylock, dir, 2048, DN_FULLPATH)))
+				strncpy(dir, fdir, 2048);
+		}
+		else
 			strncpy(dir, fdir, 2048);
+
 	if(fdest)
 		strncpy(dest, fdest, 2048);
 
