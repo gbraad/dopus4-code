7fb90f7e1f9bd445bce524d5cd908a9d37873e15
paraxenic <paraxenic@b88df514-890a-458d-8813-3e1211de039a>
2012-10-06 18:18:34 +0000

    Updated versions and dates for latest release.

--- dopus4-os4/trunk/config-os4/ConfigOpus_rev.h
+++ dopus4-os4/trunk/config-os4/ConfigOpus_rev.h
@@ -2,15 +2,15 @@
 #define REVISION		18
 #define SUBREVISION		1
 
-#define DATE			"29.09.2012"
+#define DATE			"06.10.2012"
 #define VERS			"ConfigOpus 4.18"
-#define VSTRING			"ConfigOpus 4.18 (29.09.2012)\r\n"
-#define VERSTAG			"\0$VER: ConfigOpus 4.18 (29.09.2012)"
+#define VSTRING			"ConfigOpus 4.18 (06.10.2012)\r\n"
+#define VERSTAG			"\0$VER: ConfigOpus 4.18 (06.10.2012)"
 
 #define VERSION_STR		"4"
 #define REVISION_STR		"18"
 #define SUBREVISION_STR		"1"
 
 #define NEW_VERS		"ConfigOpus 4.18.1"
-#define NEW_VSTRING		"ConfigOpus 4.18.1 (29.09.2012)\r\n"
-#define NEW_VERSTAG		"\0$VER: ConfigOpus 4.18.1 (29.09.2012)"
+#define NEW_VSTRING		"ConfigOpus 4.18.1 (06.10.2012)\r\n"
+#define NEW_VERSTAG		"\0$VER: ConfigOpus 4.18.1 (06.10.2012)"
--- dopus4-os4/trunk/program-os4/DirectoryOpus_rev.h
+++ dopus4-os4/trunk/program-os4/DirectoryOpus_rev.h
@@ -2,15 +2,15 @@
 #define REVISION		18
 #define SUBREVISION		38
 
-#define DATE			"29.09.2012"
+#define DATE			"06.10.2012"
 #define VERS			"DirectoryOpus 4.18"
-#define VSTRING			"DirectoryOpus 4.18 (29.09.2012)\r\n"
-#define VERSTAG			"\0$VER: DirectoryOpus 4.18 (29.09.2012)"
+#define VSTRING			"DirectoryOpus 4.18 (06.10.2012)\r\n"
+#define VERSTAG			"\0$VER: DirectoryOpus 4.18 (06.10.2012)"
 
 #define VERSION_STR		"4"
 #define REVISION_STR		"18"
 #define SUBREVISION_STR		"38"
 
 #define NEW_VERS		"DirectoryOpus 4.18.38"
-#define NEW_VSTRING		"DirectoryOpus 4.18.38 (29.09.2012)\r\n"
-#define NEW_VERSTAG		"\0$VER: DirectoryOpus 4.18.38 (29.09.2012)"
+#define NEW_VSTRING		"DirectoryOpus 4.18.38 (06.10.2012)\r\n"
+#define NEW_VERSTAG		"\0$VER: DirectoryOpus 4.18.38 (06.10.2012)"
