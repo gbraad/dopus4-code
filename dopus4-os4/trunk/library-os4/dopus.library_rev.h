49b673b039a0d938d016e25cf960ec7c219e5243
paraxenic <paraxenic@b88df514-890a-458d-8813-3e1211de039a>
2012-11-20 23:20:32 +0000

    Updated library revision because of changes to catalog version requirement.

--- dopus4-os4/trunk/library-os4/dopus.library_rev.h
+++ dopus4-os4/trunk/library-os4/dopus.library_rev.h
@@ -1,16 +1,16 @@
 #define VERSION			22
-#define REVISION		41
+#define REVISION		42
 #define SUBREVISION		0
 
-#define DATE			"06.10.2012"
-#define VERS			"dopus.library 22.41"
-#define VSTRING			"dopus.library 22.41 (06.10.2012)\r\n"
-#define VERSTAG			"\0$VER: dopus.library 22.41 (06.10.2012)"
+#define DATE			"20.11.2012"
+#define VERS			"dopus.library 22.42"
+#define VSTRING			"dopus.library 22.42 (20.11.2012)\r\n"
+#define VERSTAG			"\0$VER: dopus.library 22.42 (20.11.2012)"
 
 #define VERSION_STR		"22"
-#define REVISION_STR		"41"
+#define REVISION_STR		"42"
 #define SUBREVISION_STR		"0"
 
-#define NEW_VERS		"dopus.library 22.41"
-#define NEW_VSTRING		"dopus.library 22.41 (06.10.2012)\r\n"
-#define NEW_VERSTAG		"\0$VER: dopus.library 22.41 (06.10.2012)"
+#define NEW_VERS		"dopus.library 22.42"
+#define NEW_VSTRING		"dopus.library 22.42 (20.11.2012)\r\n"
+#define NEW_VERSTAG		"\0$VER: dopus.library 22.42 (20.11.2012)"
