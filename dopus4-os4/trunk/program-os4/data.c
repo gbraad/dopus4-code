8f7d52de58b6f26277b5b845928d4a17453814c2
paraxenic <paraxenic@b88df514-890a-458d-8813-3e1211de039a>
2012-12-08 15:12:50 +0000

    Added global variable for copy buffer. Added global variable for optional directory replacement method.

--- dopus4-os4/trunk/program-os4/data.c
+++ dopus4-os4/trunk/program-os4/data.c
@@ -268,6 +268,7 @@ int recurse_max_depth /*=0*/ ;				/* Maximum depth reached */
 
 struct path_complete completion[2] /*={{0},{0}}*/ ;	/* Path-name completion data */
 char *copybuffer; /* Global file copy buffer for CopyFile() in main5.c */
+BOOL replacedirs = FALSE; /* Replace dirs instead of merging */
 
 struct StringData stringdata;
 
