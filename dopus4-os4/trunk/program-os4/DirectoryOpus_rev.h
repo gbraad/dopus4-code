b64aa76878766129e229c7fdac4149a30abd08a8
paraxenic <paraxenic@b88df514-890a-458d-8813-3e1211de039a>
2012-12-08 16:50:48 +0000

    Bumped subrevision for next release.

--- dopus4-os4/trunk/program-os4/DirectoryOpus_rev.h
+++ dopus4-os4/trunk/program-os4/DirectoryOpus_rev.h
@@ -1,16 +1,16 @@
 #define VERSION			4
 #define REVISION		18
-#define SUBREVISION		39
+#define SUBREVISION		40
 
-#define DATE			"29.11.2012"
+#define DATE			"08.12.2012"
 #define VERS			"DirectoryOpus 4.18"
-#define VSTRING			"DirectoryOpus 4.18 (29.11.2012)\r\n"
-#define VERSTAG			"\0$VER: DirectoryOpus 4.18 (29.11.2012)"
+#define VSTRING			"DirectoryOpus 4.18 (08.12.2012)\r\n"
+#define VERSTAG			"\0$VER: DirectoryOpus 4.18 (08.12.2012)"
 
 #define VERSION_STR		"4"
 #define REVISION_STR		"18"
-#define SUBREVISION_STR		"39"
+#define SUBREVISION_STR		"40"
 
-#define NEW_VERS		"DirectoryOpus 4.18.39"
-#define NEW_VSTRING		"DirectoryOpus 4.18.39 (29.11.2012)\r\n"
-#define NEW_VERSTAG		"\0$VER: DirectoryOpus 4.18.39 (29.11.2012)"
+#define NEW_VERS		"DirectoryOpus 4.18.40"
+#define NEW_VSTRING		"DirectoryOpus 4.18.40 (08.12.2012)\r\n"
+#define NEW_VERSTAG		"\0$VER: DirectoryOpus 4.18.40 (08.12.2012)"
