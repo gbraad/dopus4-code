229cf0b7efbea76409053d3d978ba5492669601e
paraxenic <paraxenic@b88df514-890a-458d-8813-3e1211de039a>
2015-02-13 23:30:49 +0000

    Updated versions (subrevisions) for next release.

--- dopus4-os4/trunk/dopus_print-os4/DOpus_Print_rev.h
+++ dopus4-os4/trunk/dopus_print-os4/DOpus_Print_rev.h
@@ -1,16 +1,16 @@
 #define VERSION			4
 #define REVISION		18
-#define SUBREVISION		1
+#define SUBREVISION		2
 
-#define DATE			"06.10.2012"
+#define DATE			"13.02.2015"
 #define VERS			"DOpus_Print 4.18"
-#define VSTRING			"DOpus_Print 4.18 (06.10.2012)\r\n"
-#define VERSTAG			"\0$VER: DOpus_Print 4.18 (06.10.2012)"
+#define VSTRING			"DOpus_Print 4.18 (13.02.2015)\r\n"
+#define VERSTAG			"\0$VER: DOpus_Print 4.18 (13.02.2015)"
 
 #define VERSION_STR		"4"
 #define REVISION_STR		"18"
-#define SUBREVISION_STR		"1"
+#define SUBREVISION_STR		"2"
 
-#define NEW_VERS		"DOpus_Print 4.18.1"
-#define NEW_VSTRING		"DOpus_Print 4.18.1 (06.10.2012)\r\n"
-#define NEW_VERSTAG		"\0$VER: DOpus_Print 4.18.1 (06.10.2012)"
+#define NEW_VERS		"DOpus_Print 4.18.2"
+#define NEW_VSTRING		"DOpus_Print 4.18.2 (13.02.2015)\r\n"
+#define NEW_VERSTAG		"\0$VER: DOpus_Print 4.18.2 (13.02.2015)"
