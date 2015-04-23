f40eb788e98647c0396161fc7ba1195c272860da
paraxenic <paraxenic@b88df514-890a-458d-8813-3e1211de039a>
2015-04-23 18:15:26 +0000

    * Added missing reply port to IORequest allocation for opening printer.device. * Bumped program revision for next release. * Updated Dopus_Print history file.

--- dopus4-os4/trunk/dopus_print-os4/DOpus_Print_rev.h
+++ dopus4-os4/trunk/dopus_print-os4/DOpus_Print_rev.h
@@ -1,16 +1,16 @@
 #define VERSION			4
 #define REVISION		18
-#define SUBREVISION		3
+#define SUBREVISION		4
 
-#define DATE			"13.04.2015"
+#define DATE			"23.04.2015"
 #define VERS			"DOpus_Print 4.18"
-#define VSTRING			"DOpus_Print 4.18 (13.04.2015)\r\n"
-#define VERSTAG			"\0$VER: DOpus_Print 4.18 (13.04.2015)"
+#define VSTRING			"DOpus_Print 4.18 (23.04.2015)\r\n"
+#define VERSTAG			"\0$VER: DOpus_Print 4.18 (23.04.2015)"
 
 #define VERSION_STR		"4"
 #define REVISION_STR		"18"
-#define SUBREVISION_STR		"3"
+#define SUBREVISION_STR		"4"
 
-#define NEW_VERS		"DOpus_Print 4.18.3"
-#define NEW_VSTRING		"DOpus_Print 4.18.3 (13.04.2015)\r\n"
-#define NEW_VERSTAG		"\0$VER: DOpus_Print 4.18.3 (13.04.2015)"
+#define NEW_VERS		"DOpus_Print 4.18.4"
+#define NEW_VSTRING		"DOpus_Print 4.18.4 (23.04.2015)\r\n"
+#define NEW_VERSTAG		"\0$VER: DOpus_Print 4.18.4 (23.04.2015)"
