af784d3de27231d50bcf5761e3d9fc7b14930191
paraxenic <paraxenic@b88df514-890a-458d-8813-3e1211de039a>
2015-04-28 14:14:21 +0000

    * Fixed library version string. * Bumped library revision (22.42.2). * Bumped dopusrt revision (4.18.3)

--- dopus4-os4/trunk/library-os4/dopus.library_rev.h
+++ dopus4-os4/trunk/library-os4/dopus.library_rev.h
@@ -1,16 +1,16 @@
 #define VERSION			22
 #define REVISION		42
-#define SUBREVISION		1
+#define SUBREVISION		2
 
-#define DATE			"13.02.2015"
+#define DATE			"28.04.2015"
 #define VERS			"dopus.library 22.42"
-#define VSTRING			"dopus.library 22.42 (13.02.2015)\r\n"
-#define VERSTAG			"\0$VER: dopus.library 22.42 (13.02.2015)"
+#define VSTRING			"dopus.library 22.42 (28.04.2015)\r\n"
+#define VERSTAG			"\0$VER: dopus.library 22.42 (28.04.2015)"
 
 #define VERSION_STR		"22"
 #define REVISION_STR		"42"
-#define SUBREVISION_STR		"1"
+#define SUBREVISION_STR		"2"
 
-#define NEW_VERS		"dopus.library 22.42.1"
-#define NEW_VSTRING		"dopus.library 22.42.1 (13.02.2015)\r\n"
-#define NEW_VERSTAG		"\0$VER: dopus.library 22.42.1 (13.02.2015)"
+#define NEW_VERS		"dopus.library 22.42.2"
+#define NEW_VSTRING		"dopus.library 22.42.2 (28.04.2015)\r\n"
+#define NEW_VERSTAG		"\0$VER: dopus.library 22.42.2 (28.04.2015)"
