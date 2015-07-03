389e09a4563e90eee852da29f3dece4e87046451
paraxenic <paraxenic@b88df514-890a-458d-8813-3e1211de039a>
2015-07-03 18:50:24 +0000

    Bumped versions for all Dopus4 components.

--- dopus4-os4/trunk/dopusrt-os4/DOpusRT_rev.h
+++ dopus4-os4/trunk/dopusrt-os4/DOpusRT_rev.h
@@ -1,16 +1,16 @@
 #define VERSION			4
 #define REVISION		18
-#define SUBREVISION		3
+#define SUBREVISION		4
 
-#define DATE			"28.04.2015"
+#define DATE			"02.07.2015"
 #define VERS			"DOpusRT 4.18"
-#define VSTRING			"DOpusRT 4.18 (28.04.2015)\r\n"
-#define VERSTAG			"\0$VER: DOpusRT 4.18 (28.04.2015)"
+#define VSTRING			"DOpusRT 4.18 (02.07.2015)\r\n"
+#define VERSTAG			"\0$VER: DOpusRT 4.18 (02.07.2015)"
 
 #define VERSION_STR		"4"
 #define REVISION_STR		"18"
-#define SUBREVISION_STR		"3"
+#define SUBREVISION_STR		"4"
 
-#define NEW_VERS		"DOpusRT 4.18.3"
-#define NEW_VSTRING		"DOpusRT 4.18.3 (28.04.2015)\r\n"
-#define NEW_VERSTAG		"\0$VER: DOpusRT 4.18.3 (28.04.2015)"
+#define NEW_VERS		"DOpusRT 4.18.4"
+#define NEW_VSTRING		"DOpusRT 4.18.4 (02.07.2015)\r\n"
+#define NEW_VERSTAG		"\0$VER: DOpusRT 4.18.4 (02.07.2015)"
